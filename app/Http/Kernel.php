094a2ec255253c65899d62e6b5a8100108331dbc
Shahrukh <xixno1@gmail.com>
2020-12-08 13:11:54 +0500

    cors 4

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,9 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'Localization',
+            'bindings',
+            'cors'
         ],
     ];
 
@@ -62,6 +65,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // test
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 
     /**
