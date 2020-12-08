83d5f1c74a09a834c1e36adad8eff25735ad6ef6
Shahrukh <xixno1@gmail.com>
2020-12-08 13:07:53 +0500

    cors 4

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,8 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Origin:  http://localhost:8100')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token, Accept, Authorization');
+            ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token, Accept, Authorization, X-Requested-With');
     }
 } 
